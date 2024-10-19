28f2110e0427bf152efa15c678a65af646c8dacc
anambi82 <anambi@tamu.edu>
2024-10-19 00:59:39 -0500

    begun skeleton for job apply page

---
+++ cfg2024/app/jobs/[id]/apply/action.ts
@@ -0,0 +1,30 @@
+import { createClient } from '@/utils/supabase/server';
+
+
+export interface JobPostingData {
+  id?: number;
+  title: string;
+  description: string;
+  salary: number;
+  recruiter: string;
+  skills: string[]; // Changed from OptionType[] to string[]
+  qualification: string; // Changed from OptionType: string
+  company: string;
+}
+
+
+export async function getJob(id: number) {
+    const supabase = createClient();
+  
+    const { data, error } = await supabase
+      .from("Job")
+      .select("title, description, salary, recruiter, skills, qualification, company")
+      .eq("id", id);
+  
+    if (error) {
+      console.error(error);
+      return { success: false, error, job: [] };
+    }
+  
+    return { success: true, job: data as JobPostingData[]};
+  }
