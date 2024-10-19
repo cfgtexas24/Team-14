8af27b0303bff343e11ddd8bf18a8df97ac5c702
anambi82 <anambi@tamu.edu>
2024-10-18 21:32:22 -0500

    working on job board page

--- cfg2024/app/jobs/page.tsx
+++ cfg2024/app/jobs/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 import {
     Card,
     CardContent,
@@ -6,23 +7,40 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { job_title } from "./actions";
 
 export default function Page() {
-    return (
-        <Card className='box-border h'>
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-            <p>Card Footer</p>
-            </CardFooter>
-        </Card>
+    const [jobData, setJobData] = useState<string | null>(null);
 
-        
-    )
+    const handleClick = async () => {
+        const result = await job_title();
+
+        if (result.success) {
+            setJobData(result.data);
+            console.log(result.data);
+        } else {
+            console.error("Error fetching data:", result.error);
+        }
+    };
+
+    return (
+        <div className="flex justify-center">
+            <Card className='box-border w-1/2'>
+                <CardHeader>
+                    <CardTitle>Job Title</CardTitle>
+                    <CardDescription>Job Description</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <CardDescription className='text-color black'>Location</CardDescription>
+                </CardContent>
+                <CardFooter>
+                    <CardDescription className='text-clip overflow-hidden'>Skills</CardDescription>
+                </CardFooter>
+                <Button onClick={handleClick}>Fetch Jobs</Button>
+            </Card>
+
+        </div>
+    );
 }
