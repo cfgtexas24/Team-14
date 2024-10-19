63932a64f8277102955fb725763aa9bfd7dc6e08
Caleb Bae <baecal000@gmail.com>
2024-10-18 19:43:18 -0500

    create card for groups

--- cfg2024/app/community/groups/page.tsx
+++ cfg2024/app/community/groups/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import React from "react";
+import { Session, Chatbox } from "@talkjs/react";
+import Talk from "talkjs";
+import { useCallback } from "react";
+import { ThreeDCard } from "@/components/GroupsThreeDCard";
+
+const Match = () => {
+  const syncUser = useCallback(
+    () =>
+      new Talk.User({
+        id: "nina",
+        name: "Nina",
+        email: "nina@example.com",
+        photoUrl: "https://talkjs.com/new-web/avatar-7.jpg",
+        welcomeMessage: "Hi!",
+      }),
+    []
+  );
+
+  return (
+    <div>
+      <ThreeDCard />
+
+      <Session
+        appId="t3Z7QMmB"
+        userId="sample_user_sebastian"
+        syncUser={syncUser}
+      >
+        <Chatbox
+          conversationId="sample_group_chat"
+          style={{ width: "100%", height: "500px" }}
+        ></Chatbox>
+      </Session>
+    </div>
+  );
 };
 
-export default page;
+export default Match;
