c8372ac5f46ad85cce9eed52cd4f7db758edc538
Caleb Bae <baecal000@gmail.com>
2024-10-19 03:55:12 -0500

    Design and hero

--- cfg2024/components/MainNav.tsx
+++ cfg2024/components/MainNav.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import MainNavLinks from "./MainNavLinks";
+import Logo from "@/assets/logo.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const MainNav = () => {
   return (
     <div className="flex justify-between">
-      <span className="text-3xl">LOGO</span>
-      <div className="flex items-center gap-4">
+      <div>
+        <Link href={"/"} legacyBehavior passHref prefetch={false}>
+          <Image
+            src={Logo}
+            alt="Logo"
+            width={200}
+            height={200}
+            className="hover:cursor-pointer"
+          />
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-6">
         <MainNavLinks />
       </div>
     </div>
